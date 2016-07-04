934886e0d42206e5eb17a6398a9aab77b285f49a
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-07-04 20:05:22 +0530

    Gives all possible combinations of string
    with/without repeated elements

--- combinations.py
+++ combinations.py
@@ -2,7 +2,6 @@
 """print list(combinations('12345',2))
 [('1', '2'), ('1', '3'), ('1', '4'), ('1', '5'), ('2', '3'), ('2', '4'), ('2', '5'), ('3', '4'), ('3', '5'), ('4', '5')]"""
 
-
 from itertools import combinations
 a=input()
 l=a.split(' ')
@@ -17,3 +16,14 @@ for i in range(0,len(b),1):
     
     
     
+""""print list(combinations_with_replacement('12345',2))
+[('1', '1'), ('1', '2'), ('1', '3'), ('1', '4'), ('1', '5'), ('2', '2'), ('2', '3'), ('2', '4'), ('2', '5'), ('3', '3'), ('3', '4'), ('3', '5'), ('4', '4'), ('4', '5'), ('5', '5')]"""
+
+from itertools import combinations_with_replacement
+a=input()
+l=a.split()
+l[0]=sorted(l[0])
+l[1]=int(l[1])
+b=list(combinations_with_replacement(l[0],l[1]))
+for i in range(0,len(b),1):
+    print(*b[i],sep="")
