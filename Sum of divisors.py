52b5b2311ffa876e5b72e9b016f0a48d6f38a0a1
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-07-30 13:33:58 +0530

    Sum of divisors of a number

---
+++ Sum of divisors.py
@@ -0,0 +1,16 @@
+from math import *
+n=int(input())
+while n:
+    s=0
+    m=int(input())
+    sq=int(sqrt(m))
+    for i in range(1,sq+1,1):
+        if m%i==0:
+            s+=(i+m//i)
+        
+    if sq*sq==m:
+        print((s-sq))
+    else:
+        print(s)
+    
+    n=n-1 
