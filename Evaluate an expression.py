87fd3e99351867805124bd99f22c7d5972fc9627
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-10-14 23:30:04 +0530

    Create Evaluate an expression.py

---
+++ Evaluate an expression.py
@@ -0,0 +1,5 @@
+x,k=map(int,input().split())            
+p=eval(input())
+print(p==k)
+
+''' This code checks if p(x)==k , where p is an input expression of form    p(x) = x**4 - 1*x**3 + 5*x**2  '''
