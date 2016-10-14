6595d933143ebe953ff77f1b6413a687e9666a69
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-10-14 23:41:29 +0530

    Update Evaluate an expression.py

--- Evaluate an expression.py
+++ Evaluate an expression.py
@@ -2,4 +2,5 @@ x,k=map(int,input().split())
 p=eval(input())
 print(p==k)
 
-''' This code checks if p(x)==k , where p is an input expression of form    p(x) = x**4 - 1*x**3 + 5*x**2  '''
+''' This code checks if p(x)==k , where p is an input expression of form  p(x) = x**4 - 1*x**3 + 5*x**2  and store result as a string'''
+''' The eval() function can evaluate even the inbuilt keywords as: eval(print(abs(-1)+1)) gives 2 '''
