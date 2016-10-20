c70d2e0a60f1ea326ed5236724a6a4a7677c9972
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-10-20 22:31:22 +0530

    sort( )

---
+++ sort function.py
@@ -0,0 +1,19 @@
+'''Use of sort() and sorted() functions in python'''
+
+a=list(map(int,input().split()))
+
+a.sort(reverse=True)             #sort in descending order
+sorted(a,reverse=True)
+
+a.sort(key=len)                 #sort in accordance with length of strings
+sorted(a,key=len)
+
+a.sort(key=str.lower)           #sort by treating all elements as lower case
+sorted(a,key=str.lower)              #a similar is str.upper
+
+a.sort(key=func)                #key can accept any user defined function
+sorted(a,key=func)              #this func must return a value to guide sorting
+
+        #Eg: def func(s):
+        #        return s[-3]     #returns third last character of string 
+                                  #sorts acc to third last character
