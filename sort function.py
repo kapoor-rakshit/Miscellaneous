70d8274f33186d74644cf5e6261af50aca2e4b0d
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-12-17 19:21:11 +0530

    Update sort function.py

--- sort function.py
+++ sort function.py
@@ -5,7 +5,7 @@ a=list(map(int,input().split()))
 
 a.sort(reverse=True)             #sort in descending order
 b=sorted(a,reverse=True)
-
+                                #key always accept a callable function (can be by lambda or def)
 a.sort(key=len)                 #sort in accordance with length of strings
 b=sorted(a,key=len)
 
@@ -32,3 +32,18 @@ print(*(sorted(a,key=lambda character:(character.isdigit() and int(character)%2=
 #sorts acc to key values where parametrs to lambda are: first param is given higher priority,then second param and so on.
 #higher the priority means it will be placed at last ie here first will be lower,upper,odd digit,even digit.
 #the last argument is a variable which specifies that sort acc to caracters of string.
+
+'''sort the matrix rows according to kth column ie rows are adjusted preserving their elements wrt to kth element of each row''' 
+n,m=map(int,input().split())
+mat=[]
+for i in range(0,n,1):
+    tp=[]
+    tp=list(map(int,input().split()))
+    mat.append(tp)
+k=int(input())
+mat.sort(key=lambda element:element[k])    #lambda returns the kth element of each element(lists or rows here) in every row
+for i in range(0,n,1):                     #it is applied to every row to get kth element as a key
+    print(*mat[i])
+
+    
+    
