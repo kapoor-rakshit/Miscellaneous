1b6b154c4a6398cd016ed1da99a22a6131635b3b
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-12-20 23:39:33 +0530

    Update numpy_package.py

--- numpy_package.py
+++ numpy_package.py
@@ -9,7 +9,7 @@ print(b)
 #They are similar to lists, except that every element of an array must be the same type.
 #numpy.array() is used to convert a list into a NumPy array. 
 #The second argument (float) can be used to set the type of array elements.
-
+'''shape and reshape'''
 import numpy
 a=list(map(int,input().split()))
 a=numpy.array(a)
@@ -19,9 +19,20 @@ print(a.shape)                               # to get array dimensions #(5,)->5 
 a.shape=(3,3)                                # to change array dimensions
 print(a)
 
-print(numpy.reshape(a,(3,3)))                #It creates a new array and does not modify the original array itself.
+print(numpy.reshape(a,(3,3)))                # It creates a new array and does not modify the original array itself.
 
 #The shape tool gives a tuple of array dimensions and can be used to change the dimensions of an array.
 #   [[1 2]
 #    [3 4]
 #    [5 6]]
+'''transpose and flatten'''
+import numpy
+n,m=map(int,input().split())
+t=[]
+for i in range(0,n,1):
+    a=list(map(int,input().split()))
+    t.append(a)
+print(numpy.transpose(t))         # transposition of an array using the tool numpy.transpose. 
+                                  # It will not affect the original array, but it will create a new array.
+    
+print(numpy.array(t).flatten())   # The tool flatten creates a copy of the input array flattened to one dimension.
