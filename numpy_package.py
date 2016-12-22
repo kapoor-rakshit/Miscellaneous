ca99ab4cea0dac16d288a5a438d9b3cc5bffe606
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-12-22 23:57:54 +0530

    Update numpy_package.py

--- numpy_package.py
+++ numpy_package.py
@@ -127,3 +127,15 @@ print(numpy.std(a,axis=None))
 #The var tool computes the arithmetic variance along the specified axis.
 '''Variance:average of squared differences.'''
 '''(squared difference):(subtract the Mean and individual no.s) square the result.'''
+
+'''linear algebra (linalg) sub-module'''
+print(numpy.linalg.det([[1 , 2], [2, 1]]))      #Output : -3.0
+#The linalg.det tool computes the determinant of an array.
+vals, vecs = numpy.linalg.eig([[1 , 2], [2, 1]])
+print(vals)                                      #Output : [ 3. -1.]
+print(vecs)                                      #Output : [[ 0.70710678 -0.70710678]
+#The linalg.eig computes the eigenvalues and right eigenvectors of a square array.
+print(numpy.linalg.inv([[1 , 2], [2, 1]]))       #Output : [[-0.33333333  0.66666667]
+                                                #          [ 0.66666667 -0.33333333]]
+#The linalg.inv tool computes the (multiplicative) inverse of a matrix.
+                                                #          [ 0.70710678  0.70710678]]
