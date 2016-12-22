7345dc1d1e49cb4f3c0d0bee3bc3ece8f10db2d5
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2016-12-23 00:00:30 +0530

    Update numpy_package.py

--- numpy_package.py
+++ numpy_package.py
@@ -134,8 +134,8 @@ print(numpy.linalg.det([[1 , 2], [2, 1]]))      #Output : -3.0
 vals, vecs = numpy.linalg.eig([[1 , 2], [2, 1]])
 print(vals)                                      #Output : [ 3. -1.]
 print(vecs)                                      #Output : [[ 0.70710678 -0.70710678]
+                                                 #         [ 0.70710678  0.70710678]]
 #The linalg.eig computes the eigenvalues and right eigenvectors of a square array.
 print(numpy.linalg.inv([[1 , 2], [2, 1]]))       #Output : [[-0.33333333  0.66666667]
                                                 #          [ 0.66666667 -0.33333333]]
 #The linalg.inv tool computes the (multiplicative) inverse of a matrix.
-                                                #          [ 0.70710678  0.70710678]]
