41e93aa131c8b9115464e9404fc30100f396ceff
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2017-06-05 22:07:53 +0530

    exception handling

---
+++ errors_exceptions_python.py
@@ -0,0 +1,16 @@
+n=int(input())
+while n:
+    a,b=input().split()
+    try:
+        print(int(a)//int(b))
+    except ZeroDivisionError as v:
+        print("Error Code:",v)
+    except ValueError as v:
+        print("Error Code:",v)
+    n=n-1
+ --------------------------------------OR------------------------------------
+ try:
+     print(int(a)//int(b))
+ except (ZeroDivisionError, ValueError, NameError, TypeError, RuntimeError) as v:
+     print("Error Code:",v)
+ 
