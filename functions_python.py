f38b734a5dcafffc62a6c315fd25f317255916eb
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2017-06-05 17:36:59 +0530

    variable length arguments functions

---
+++ functions_python.py
@@ -0,0 +1,21 @@
+# *tupleofargs is used to send a non-keyworded variable length argument tuple to the function.
+# **dictofargs allows you to pass keyworded variable length of arguments to a function. 
+# Use **dictofargs if you want to handle named arguments in a function.
+
+# IMPORTANT : Order of using *args **kwargs and formal args is
+# some_func(farg1,farg2,*args,**kwargs)
+
+
+def first(*tupleofargs,**dictofargs):
+    an=1
+    for i in tupleofargs:
+        an/=i
+        print(an)
+    for key,value in dictofargs.items():                #use dict.iteritems() for python 2.x to iterate a dictionary
+        print(key,":",value)
+
+
+if __name__ == '__main__':
+    first(2,3,4,naam="kapoor-rakshit")
+    print()
+    first(2,pata="amritsar",intern="true at WIPRO")
