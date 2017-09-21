5a21082d8bb22f553ad5bfc5834df3dd9f2c250b
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2017-09-21 21:56:19 +0530

    added comment

--- squares in range.py
+++ squares in range.py
@@ -1,3 +1,6 @@
+#NOTE : perfect squares always have odd no. of divisors
+#       this prog will find no. of perfect squares (or those with odd no. of divisors) in the given range
+
 from math import *
 t=int(input())
 while t:
