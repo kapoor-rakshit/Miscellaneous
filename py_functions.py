5ac63b355ef23301fdd66dc85b2f81a811610174
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2018-04-17 13:59:21 +0530

    Update and rename functions_python.py to py_functions.py

--- py_functions.py
+++ py_functions.py
@@ -13,9 +13,13 @@ def first(*tupleofargs,**dictofargs):
         print(an)
     for key,value in dictofargs.items():                #use dict.iteritems() for python 2.x to iterate a dictionary
         print(key,":",value)
-
+        
+        
+def defarg(x, y=9)                                     #use of y=9 when second arg for y not supplied in func call
 
 if __name__ == '__main__':
     first(2,3,4,naam="kapoor-rakshit")
     print()
     first(2,pata="amritsar",intern="true at WIPRO")
+
+    
