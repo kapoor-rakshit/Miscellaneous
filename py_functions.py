9898602c445b3f221e195d93b461056de344bd83
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2018-04-19 10:18:34 +0530

    Update py_functions.py

--- py_functions.py
+++ py_functions.py
@@ -17,9 +17,30 @@ def first(*tupleofargs,**dictofargs):
         
 def defarg(x, y=9)                                     #use of y=9 when second arg for y not supplied in func call
 
+def chk(firstarg, secondarg, thirdarg)                 # args can be passed in any order by user
+
+def retchk(n):
+    return mx,mn,l                                     # multiple vals return 
+
+def docchk():
+    """ this is func doc """
+
+
 if __name__ == '__main__':
     first(2,3,4,naam="kapoor-rakshit")
     print()
     first(2,pata="amritsar",intern="true at WIPRO")
 
+    chk(thirdarg=89, firstarg=90, secondarg=84)        # user defined argument pass order
+    
+    print(retchk(n))                                   # return multiple values as a tuple
+    m,n,l = retchk(n)                                  # multiple vals returned to corresponding variables
+    
+    isinstance(funcname, object)                       # boolean for funcname is object
+    
+    id(funcname)                                       # return location id for func
+    
+    funcname.func_doc                                  # function documentation, written as comment as firstline in definition
+    
+    funcname.func_name
     
