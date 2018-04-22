7d2e6558c98a572626d27267aa779c6f9ea21cfb
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2018-04-22 14:07:34 +0530

    Update py_hacks.py

--- py_hacks.py
+++ py_hacks.py
@@ -2,10 +2,35 @@
 import sys
 
 naam, arg1, arg2 = sys.argv     # first val in argv is filename, and remaining values are supplied by user
+arg2 = sys.argv[2]              # 2 index arg in argv
 
 print(naam)
 print(arg1)
 print(arg2)
+
+
+# sys module
+sys.platform                  # platform details
+sys.getwindowsversion()       # windows version
+sys.version                   # python version
+sys.path                      # python path
+sys.builtin_module_names      # name of builtin modules
+sys.copyright                 # copyright of python
+sys.executable                # location of python interpreter
+sys.exit()                    # exit
+sys.getsizeof                 # memory used in bytes for current interpreter
+sys.modules                   # all loaded modules
+
+
+# os module
+os.name                      # name of operating system (not for Windows)
+os.environ                   # dict for env variables
+os.getcwd()
+os.listdir()
+os.mkdir()
+os.rmdir()
+os.remove()
+os.system('notepad')        # execute command from terminal , os.system('calc')
 
 
 # escape sequence
